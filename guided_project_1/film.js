c34678b3f3e4620cc543b87acb506cb897a2eda5
emolden <emolden@travelers.com>
2025-03-03 15:20:22 -0500

    film.js is fetching all data needed for the page

--- guided_project_1/film.js
+++ guided_project_1/film.js
@@ -1,1 +1,22 @@
-console.log('hello world')
+const sp = new URLSearchParams(window.location.search);
+const id = sp.get('id');
+
+fetch(`http://localhost:9001/api/films/${id}`)
+    .then(response => response.json())
+    .then(data => updateFilmInfo(data))
+    .catch((error) => console.error('Error: ', error));
+
+    fetch(`http://localhost:9001/api/films/${id}/characters`)
+    .then(response => response.json())
+    .then(data => console.log('GET: ', data))
+    .catch((error) => console.error('Error: ', error));
+
+    fetch(`http://localhost:9001/api/films/${id}/planets`)
+    .then(response => response.json())
+    .then(data => console.log('GET: ', data))
+    .catch((error) => console.error('Error: ', error));
+
+    function updateFilmInfo (filmObject) {
+        console.log(filmObject);
+        console.log('release date: ', filmObject.release_date);
+    }
