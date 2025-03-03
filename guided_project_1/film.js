4fcd86013247c5f9fac3da9c99c3a14bd6cb7cc2
emolden <emolden@travelers.com>
2025-03-03 15:49:34 -0500

    characters and planets loading on the dom

--- guided_project_1/film.js
+++ guided_project_1/film.js
@@ -6,18 +6,32 @@ fetch(`http://localhost:9001/api/films/${id}`)
     .then(data => updateFilmInfo(data))
     .catch((error) => console.error('Error: ', error));
 
-    fetch(`http://localhost:9001/api/films/${id}/characters`)
+fetch(`http://localhost:9001/api/films/${id}/characters`)
     .then(response => response.json())
-    .then(data => console.log('GET: ', data))
+    .then(data => updateCharacters(data))
     .catch((error) => console.error('Error: ', error));
 
-    fetch(`http://localhost:9001/api/films/${id}/planets`)
+fetch(`http://localhost:9001/api/films/${id}/planets`)
     .then(response => response.json())
-    .then(data => console.log('GET: ', data))
+    .then(data => updatePlanets(data))
     .catch((error) => console.error('Error: ', error));
 
     function updateFilmInfo (filmObject) {
-        console.log(filmObject);
-        console.log('release date: ', filmObject.release_date);
-        document.querySelector('#release_date').innerHTML = filmObject.release_date;
+        document.querySelector('#release_date').innerHTML+= filmObject.release_date;
+        document.querySelector('#film').innerHTML = filmObject.title;
+        document.querySelector('#director').innerHTML += filmObject.director;
+        document.querySelector('#episode').innerHTML+= filmObject.episode_id;
+
+    }
+
+    function updateCharacters (characterArray) {
+        for(character of characterArray) {
+            document.querySelector('#characters').innerHTML += `<li>${character.name}</li>`
+        }
+    }
+    
+    function updatePlanets(planetArray) {
+        for(planet of planetArray) {
+            document.querySelector('#planets').innerHTML += `<li>${planet.name}</li>`
+        }
     }
